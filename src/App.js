bad22718771c9c0ee00fd1bf35ee302c2cb21c88
Marc Majcher <majcher@gmail.com>
2020-01-24 12:07:36 -0600

    looking good

--- src/App.js
+++ src/App.js
@@ -8,11 +8,31 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
+      currentFacts: [],
       facts,
+      freeCenter: true,
       showSettings: false,
       title: 'Welcome to Flatiron',
-      freeCenter: true,
     };
+  }
+
+  getNewFacts() {
+    this.setState({
+      currentFacts: this.getShuffledArray(this.state.facts).slice(0, 25),
+    });
+  }
+
+  getShuffledArray(array) {
+    const a = array.slice(0);
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  componentDidMount() {
+    this.getNewFacts();
   }
 
   render() {
@@ -20,11 +40,17 @@ export default class App extends Component {
       <div className="App">
         <nav>
           <h1>{this.state.title}</h1>
-          <ion-icon name="refresh"></ion-icon>
-          <ion-icon name="settings"></ion-icon>
+          <section class="controls">
+            <ion-icon
+              size="small"
+              name="refresh"
+              onClick={() => this.getNewFacts()}
+            ></ion-icon>
+            <ion-icon size="small" name="settings"></ion-icon>
+          </section>
         </nav>
 
-        <BingoGrid facts={this.state.facts} />
+        <BingoGrid facts={this.state.currentFacts} />
         {this.state.showSettings ? <Settings /> : ''}
       </div>
     );
--- src/BingoGrid.js
+++ src/BingoGrid.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react';
 
 export default class BingoGrid extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentFacts: [],
-    };
-  }
-
-  getNewFacts() {
-    const facts = this.getShuffledArray(this.props.facts).slice(0, 25);
-    this.setState({ currentFacts: facts });
-  }
-  getShuffledArray(array) {
-    const a = array.slice(0);
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-  }
-
-  componentDidMount() {
-    this.getNewFacts();
-  }
-
   render() {
     return (
       <div className="bingo-grid">
-        {this.state.currentFacts.map(fact => (
-          <li key="{fact}" class="fact">{fact}</li>
+        {this.props.facts.map(fact => (
+          <li key="{fact}" class="fact">
+            {fact}
+          </li>
         ))}
       </div>
     );
